53abda237169f2dd366a03d0f946498502c2a912
AmeenWaheed <ameenwaheed93@gmail.com>
2024-03-21 01:02:09 +0200

    Updated

--- main1.py
+++ main1.py
@@ -2,16 +2,12 @@ from pytube import YouTube, Playlist
 import pyfiglet
 import termcolor
 import os
-import datetime
-from os import name
+
 
 def clear_screen():
-    if name == "nt":
-        return os.system("cls")
-    else:
-        return os.system("clear")
+    return os.system("clear")
 
-clear_screen()
+
 print(termcolor.colored(pyfiglet.figlet_format("YT.DOWNLOADER"), color="red"))
 print(termcolor.colored("-" * 80, color="blue"))
 
@@ -21,24 +17,18 @@ print(end="\n")
 print(termcolor.colored("Design By: ", color="red"), end=" ")
 print(termcolor.colored("Mano!", color="yellow"))
 print(end="\n")
-print("_" * 100)
-print("_" * 100)
-print(termcolor.colored(datetime.datetime.now().strftime("%a, %d %B %Y, %H:%M:%S"), color="blue"))
-print("_" * 100)
-print("_" * 100)
-
 
 save_path = input(
     termcolor.colored("Enter your path to save videos: ", color="blue")
 ).strip()
-# clear_screen()
+clear_screen()
 
 
 def main():
     url = input(
         termcolor.colored("Enter Youtube Url Video or Playlist: ", color="red")
     ).strip()
-    # clear_screen()
+    clear_screen()
 
     if "playlist" in url.lower():
         download_playlist(url)
@@ -53,30 +43,20 @@ def download_video(url):
     print(termcolor.colored("Downloading..", color="green"), video.title)
     stream.download(save_path)
     print(termcolor.colored("Download Completed!", color="green"))
-    # clear_screen()
+    clear_screen()
 
 
 def download_playlist(url):
     playList = Playlist(url)
-    total_video = len(playList)
-    counter = 0
-
-    print(termcolor.colored("Total Videos is: ", color="blue"),": ", termcolor.colored(total_video, color="grey"))
 
     for video in playList.video_urls:
         yt = YouTube(video)
 
         stream = yt.streams.get_highest_resolution()
-
-        print(termcolor.colored("Downloading: ", color="green"), termcolor.colored(yt.title, color="grey"))
+        print(termcolor.colored("Downloading..", color="green"), yt.title)
         stream.download(save_path)
-
-        counter += 1
-
-        print(termcolor.colored(f'{counter}/{total_video}', color="yellow"))
-
-    print(termcolor.colored("Download Completed!", color="green"))
-
+        print(termcolor.colored("Download Completed!", color="green"))
+        clear_screen()
 
 
 if __name__ == "__main__":
